e90278a93e1e7b2d3c22c6f22741963104889733
Robin Stenskytt <robin.olivemark@gmail.com>
2022-11-28 14:23:53 +0100

    init

---
+++ src/main/java/se/robinsgalenskaper/alm/AlMtimeApplication.java
@@ -0,0 +1,13 @@
+package se.robinsgalenskaper.alm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlMtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlMtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/robinsgalenskaper/alm/AlMtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package se.robinsgalenskaper.alm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlMtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
